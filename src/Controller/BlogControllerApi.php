10ff808f0087b7811b51092269e4495af7874c81
leloucho <eric.hurtado@estudiantat.upc.edu>
2022-12-14 15:42:10 +0100

    homepage heroku

--- src/Controller/BlogControllerApi.php
+++ src/Controller/BlogControllerApi.php
@@ -32,7 +32,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\DateTime;
 
-#[Route('/api')]
+#[Route('/')]
 class BlogControllerApi extends AbstractController
 {
     #[Route('/', defaults: ['page' => '1'], methods: ['GET'], name: 'blog_index')]
